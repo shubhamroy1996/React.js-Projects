810eacc417110b82577b2fd5a7a56b2650759b04
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2024-05-20 10:55:34 +0530

    Merge branch 'main' of https://github.com/shubhamroy1996/React.js-Projects
    # Conflicts:
    #	Learning-2/src/App.jsx
    #	Learning-2/src/components/Header.jsx

--- Learning-2/src/components/TabButton.jsx
+++ Learning-2/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({children, onselect, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
     return (
        <li>
-        <button className ={isSelected ? 'active' : undefined} onClick={onselect}>
+        <button className ={isSelected ? 'active' : undefined} {...props}>
             {children}
         </button>
        </li>
