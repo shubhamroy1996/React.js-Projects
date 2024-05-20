1e714be33d10e95332d5bc0bd549f0435070d772
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2024-05-20 22:04:33 +0530

    add: initial add of tic-tac-toe project using reactjs

---
+++ tic-tac-toe-/src/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../public/game-logo.png'
+
+export default function Header(){
+    return(
+        <>
+        <header>
+            <h1>Tic-Tac-Toe</h1>
+            <img  src={logo} alt="logo"/>
+        </header>
+        </>
+    )
+}
