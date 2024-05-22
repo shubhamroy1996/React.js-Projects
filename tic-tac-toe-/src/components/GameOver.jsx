1c0cfbc8caa74c74ef10487fa169f9cc442e7ca6
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2024-05-22 23:03:24 +0530

    add: update game over popup

--- tic-tac-toe-/src/components/GameOver.jsx
+++ tic-tac-toe-/src/components/GameOver.jsx
@@ -1,10 +1,10 @@
-export default function GameOver({winner}) {
+export default function GameOver({winner, onRestart}) {
 return (
     <div id="game-over">
         <h2>Game Over !!!</h2>
-        {winner && <p> {winner} Won</p>}
-        {!winner && <p> Oop's It's a Draw </p>}
-        <p><button>Rematch</button></p>
+        {winner && <p> {winner} Won !!!</p>}
+        {!winner && <p> Oop's It's a Draw  </p>}
+        <p><button onClick={onRestart}>Rematch</button></p>
 
     </div>
 )
