79cdc6de5b770aafb4ee556aae62084864b52606
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2024-09-05 20:05:20 +0530

    update button onclick

--- Project-Management/src/App.jsx
+++ Project-Management/src/App.jsx
@@ -1,12 +1,35 @@
-import NewProject from "./components/NewProject";
-import ProjectSidebar from "./components/ProjectSidebar";
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <ProjectSidebar/>
-      <NewProject  />
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectSidebar onStartAddProject={handleStartAddProject} />
+        {/* <NewProject /> */}
+        {content}
+      </main>
     </>
   );
 }
--- Project-Management/src/components/ProjectSidebar.jsx
+++ Project-Management/src/components/ProjectSidebar.jsx
@@ -1,5 +1,6 @@
-export default function ProjectSidebar() {
-    
+import Button from "./Button.jsx";
+
+export default function ProjectSidebar({onStartAddProject}) {
   return (
     <>
       <aside className="w-2/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -8,19 +9,9 @@ export default function ProjectSidebar() {
         </h2>
 
         <div>
-          <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-            + Add Project
-          </button>
+          <Button onClick={onStartAddProject} children="+ Add Project" />
         </div>
-        <ul>
-          
-          <li><input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-700 focus:outline-none focus:border-stone-600 border-spacing-1" /></li>
-          <li><input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" /></li>
-          <br />
-          <li><input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" /></li>
-
-
-        </ul>
+        <ul></ul>
       </aside>
     </>
   );
