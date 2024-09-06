c3f69987a045a34149ee739f07156f49ad61b9f8
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2024-09-06 23:17:00 +0530

    add dialog and cancel button

--- Project-Management/src/components/ProjectSidebar.jsx
+++ Project-Management/src/components/ProjectSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button.jsx";
 
-export default function ProjectSidebar({onStartAddProject}) {
+export default function ProjectSidebar({ onStartAddProject, projects }) {
   return (
     <>
       <aside className="w-2/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -11,7 +11,15 @@ export default function ProjectSidebar({onStartAddProject}) {
         <div>
           <Button onClick={onStartAddProject} children="+ Add Project" />
         </div>
-        <ul></ul>
+        <ul>
+          {projects.map((project) => (
+            <li key={project.id}>
+              <button className="w-full px-2 py-1 text-left rounded-sm my-1  hover:bg-stone-600 hover:text-stone-100 text-stone-400 ">
+                {project.title}
+              </button>
+            </li>
+          ))}
+        </ul>
       </aside>
     </>
   );
